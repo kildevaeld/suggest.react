e06bb9e84c142b72e7db06118d3c45d3b6a7b4e1
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2020-05-05 16:28:23 +0200

    restructure props

--- src/base.tsx
+++ src/base.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { forwardRef, Ref } from 'react';
 import { KeyCode } from './keycodes';
 import { omit } from './util';
+import { SuggestProps } from './suggest';
 
 
 export interface Suggestion<T> {
@@ -16,9 +17,13 @@ export interface BaseSuggestSuggestionsProps<T> {
 }
 
 export interface BaseSuggestionInputProps {
-    value: string;
-    onChange: React.ChangeEventHandler<HTMLInputElement>,
-    onKeyDown: React.KeyboardEventHandler<HTMLInputElement>
+    props: {
+        value: string;
+        onChange: React.ChangeEventHandler<HTMLInputElement>,
+        onKeyDown: React.KeyboardEventHandler<HTMLInputElement>
+        onBlur?: (event: React.FocusEvent<HTMLInputElement>) => any,
+        onFocus?: (event: React.FocusEvent<HTMLInputElement>) => any,
+    }
     onClear: () => any;
 }
 
@@ -28,12 +33,14 @@ export interface BaseSuggestProps<T> {
     index: number;
     onIndex: (index: number) => any;
     onChange: React.ChangeEventHandler<HTMLInputElement>
-    onSelect: (selected: T | undefined) => any;
+    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => any;
+    onFocus?: (event: React.FocusEvent<HTMLInputElement>) => any;
+    onSelect: (selected: T | undefined, index?: number) => any;
     onClear: () => any;
 
     suggestions: T[];
     clearOnEscape?: boolean;
-    children: (props: BaseSuggestSuggestionsProps<T>) => JSX.Element;
+    children: (props: BaseSuggestSuggestionsProps<T>) => JSX.Element | null;
     // Finer grained config
     className?: string;
     style?: React.StyleHTMLAttributes<HTMLDivElement>
@@ -41,61 +48,67 @@ export interface BaseSuggestProps<T> {
     showSuggestions?: boolean;
 }
 
-function _renderInput(props: BaseSuggestionInputProps) {
-    return <input {...omit(props, ['onClear'])} data-testid="input" />
+function _renderInput({ props }: BaseSuggestionInputProps, ref?: Ref<HTMLDivElement>) {
+    return <input {...props} data-testid="input" />
 }
 
-export function SuggestBase<T>(props: BaseSuggestProps<T>) {
+
+type SuggestBaseComponent = <T = any>(props: BaseSuggestProps<T> & React.RefAttributes<any>) => JSX.Element | null;
+
+export const SuggestBase = forwardRef(function SuggestBase<T = any>(props: BaseSuggestProps<T>, ref: any) {
 
     const { value, onChange, index, onIndex,
-        onSelect, clearOnEscape = false, onClear,
+        onSelect, clearOnEscape = false, onClear, onBlur, onFocus,
         suggestions, className, renderInput = _renderInput,
         showSuggestions = !!suggestions.length,
         children, style, selected } = props;
 
-
     const inputProps: BaseSuggestionInputProps = {
-        value,
-        onChange,
         onClear,
-        onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => {
-            switch (e.keyCode) {
-                case KeyCode.Up:
-                    if (index > 0) {
-                        onIndex(index - 1);
-                    }
-                    break;
-                case KeyCode.Down:
-                    if (index < suggestions.length - 1) {
-                        onIndex(index + 1);
-                    }
-                    break;
-                case KeyCode.Tab:
-                    if (e.shiftKey) {
+        props: {
+            value,
+            onChange,
+            onFocus, onBlur,
+            onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => {
+                switch (e.keyCode) {
+                    case KeyCode.Up:
                         if (index > 0) {
                             onIndex(index - 1);
                         }
-                    } else {
-                        if (index > suggestions.length - 1) {
-                            onIndex(index + 1)
+                        break;
+                    case KeyCode.Down:
+                        if (index < suggestions.length - 1) {
+                            onIndex(index + 1);
                         }
-                    }
-                    break
-                case KeyCode.Escape:
-                    if (!!clearOnEscape)
-                        onClear();
-                    break;
-                case KeyCode.Return:
-                    if (index >= 0 && suggestions.length > 0 && showSuggestions) {
-                        e.preventDefault();
-                        onSelect?.(suggestions[index!]);
-                    }
-                    break;
+                        break;
+                    case KeyCode.Tab:
+                        if (e.shiftKey) {
+                            if (index > 0) {
+                                onIndex(index - 1);
+                            }
+                        } else {
+                            if (index > suggestions.length - 1) {
+                                onIndex(index + 1)
+                            }
+                        }
+                        break
+                    case KeyCode.Escape:
+                        if (!!clearOnEscape)
+                            onClear();
+                        break;
+                    case KeyCode.Return:
+                        if (index >= 0 && suggestions.length > 0 && showSuggestions) {
+                            e.preventDefault();
+                            onSelect?.(suggestions[index!]);
+                        }
+                        break;
+                }
             }
         }
     };
 
-    return <div className={className} style={style}>
+
+    return <div className={className} style={style} ref={ref}>
         {renderInput(inputProps)}
         {showSuggestions && children({
             suggestions: suggestions.map((m, i) => ({
@@ -104,10 +117,10 @@ export function SuggestBase<T>(props: BaseSuggestProps<T>) {
                 isSelected: i == selected
             })),
             query: value,
-            onSelect: (index) => onSelect(suggestions[index])
+            onSelect: (index) => onSelect(suggestions[index], index)
         })}
     </div>
 
 
-}
+}) as SuggestBaseComponent;
 
