16fde111b7e60113796202b96bbbda3fd36cd008
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2020-05-05 16:50:55 +0200

    build script tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,40 @@
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
+import babel from '@rollup/plugin-babel';
 
 export default {
     input: 'src/index.tsx',
     output: [{
         file: pkg.browser,
-        format: 'umd',
+        format: 'cjs',
         name: 'suggest',
         //dir: "./dist",
-        globals: {
-            'react': 'React',
-            'react-dom': 'ReactDOM'
-        }
+        // globals: {
+        //     'react': 'React',
+        //     'react-dom': 'ReactDOM',
+
+        // }
     }, {
         file: pkg.module,
         //dir: "./dist",
         format: 'es',
     }],
     external: [
-        'react', 'react-dom',
+        'react', 'react-dom', 'use-scroll-lock'
     ],
     plugins: [
-        //babel(),
+        babel({
+            babelHelpers: 'bundled'
+        }),
         typescript({
             declaration: false,
             tsconfig: false,
             allowSyntheticDefaultImports: true,
             jsx: 'react',
         })
+        // resolve({
+        //     mainFields:
+        // }),
+        // commonjs(),
     ]
 };
